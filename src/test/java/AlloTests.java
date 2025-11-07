e3e713995940a1f8aabda69049caed0d5f9fa2ee
MacZone <Butkovova1995@gmail.com>
2025-11-07 15:26:26 +0200

    add class with tests

---
+++ src/test/java/AlloTests.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+
+public class AlloTests {
+
+    @Test
+    public void testFullScenario() throws InterruptedException {
+
+        String expTitleHomePage = "YouTube";
+        int fourSymbols = 4;
+        int secondSuggestionField = 1;
+        int fourthVideo = 3;
+
+
+        String expectedName = "Каталог";
+
+    }
+
+}
